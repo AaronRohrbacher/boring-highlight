502d07994eafa6d4291ffa4f63abacaf27ebf45c
Aaron Rohrbacher and Senya Webster <aaron@>
2017-08-08 11:17:29 -0700

    added highlight feature to specific paragraphs

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#p1").click(function() {
+    $("#p1").addClass("highlight");
+  });
+  $("#p2").click(function() {
+    $("#p2").addClass("highlight");
+  });
+});
